54da8eb484fc8d5c3d50c35c67d8f42e980cb03d
huy-tap-tanh <nhathuy05122005@gmail.com>
2026-08-19 21:07:27 +0700

    b3.sangmvc

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ const path = require("path");
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware để xử lý dữ liệu từ form
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+// Middleware để xử lý dữ liệu JSON
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
+
 //template enginer
 app.engine(
   "hbs",
@@ -26,10 +38,19 @@ app.set("views", path.join(__dirname, "resouses", "views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+// newss
 app.get("/newsss", (req, res) => {
   res.render("newss");
 });
-// console.log("pasth", path.join(__dirname, "resoueses", "views"));
+// seacher
+app.get("/seacher", (req, res) => {
+  res.render("seacher");
+});
+// seacher
+app.post("/seacher", (req, res) => {
+  console.log(req.body);
+  res.send("");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
